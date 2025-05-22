739194db86b4530e1d872e49010ca8b179ec25c8
Dinh Quoc Tri <tridq6280@ut.edu.vn>
2025-05-22 23:51:57 +0700

    sua SFML

--- client.cpp
+++ client.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <SFML/Graphics.hpp>
+
 #pragma comment(lib, "ws2_32.lib")
 #define PORT 2808
 #define SERVER_ADDR "127.0.0.1"
@@ -8,6 +10,7 @@
 using namespace std;
 
 int main() {
+    // Khởi tạo Winsock
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         cerr << "Khoi tao Winsock that bai: " << WSAGetLastError() << endl;
@@ -23,7 +26,7 @@ int main() {
 
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
-    inet_pton(AF_INET, SERVER_ADDR, &serverAddr.sin_addr); // Sử dụng inet_pton thay inet_addr
+    inet_pton(AF_INET, SERVER_ADDR, &serverAddr.sin_addr);
     serverAddr.sin_port = htons(PORT);
 
     if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
@@ -44,33 +47,42 @@ int main() {
     }
 
     char buffer[1024];
-    int bytesReceived = recv(clientSocket, buffer, 1023, 0); // Giảm 1 byte để chừa chỗ cho '\0'
+    int bytesReceived = recv(clientSocket, buffer, 1023, 0);
     if (bytesReceived > 0) {
         buffer[bytesReceived] = '\0';
         cout << "Du lieu nhan duoc tu server: " << buffer << endl;
-    }
-    else {
+    } else {
         cerr << "Khong nhan duoc du lieu: " << WSAGetLastError() << endl;
     }
 
     closesocket(clientSocket);
     WSACleanup();
-    return 0;
-}
 
-
-#include <SFML/Graphics.hpp>
-#include <iostream>
-
-int main() {
-    // SFML window
+    // SFML Giao diện 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Vong Quay May Man");
-    // wheel static
-    sf::circleShape wheel(200);
-    wheel.setFillColor(sf::yellow::green);
-    wheel.setPosition(300, 200);
-    //spin button
-    sf::Reactangleshape spinbutton(sf::vector2f(100, 50));
-    spinbutton.setFillColor(sf::Color::Red);
-    spinbutton.setPosition(350, 500);
+
+    // Hình tròn đại diện vòng quay
+    sf::CircleShape wheel(200);
+    wheel.setFillColor(sf::Color::Green);
+    wheel.setPosition(300, 100); 
+
+    // Nút "Spin"
+    sf::RectangleShape spinButton(sf::Vector2f(100, 50));
+    spinButton.setFillColor(sf::Color::Red);
+    spinButton.setPosition(350, 500);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::White);
+        window.draw(wheel);
+        window.draw(spinButton);
+        window.display();
+    }
+
+    return 0;
 }
