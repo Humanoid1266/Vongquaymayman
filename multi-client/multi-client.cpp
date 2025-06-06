24489b37e1399bdeae576993d1aaf09cd3be6953
tramntb1633 <tramntb1633@gmail.com>
2025-06-06 08:25:23 +0700

    multi

--- multi-client/multi-client.cpp
+++ multi-client/multi-client.cpp
@@ -7,10 +7,12 @@
 #define PORT 2808 
 using namespace std;
 
-int generateReward() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 5);
-    int rewards[] = {100, 200, 300, 0, 50, 150};
-    return rewards[dis(gen)];
+while (true) {
+    SOCKET clientSocket = accept(serverSocket, NULL, NULL);
+    if (clientSocket == INVALID_SOCKET) {
+        cerr << "Chap nhan ket noi khong thanh cong" << WSAGetLastError() << endl;
+        continue;
+    }
+    cout << "Client moi ket noi!" << endl;
+    clientThreads.emplace_back(thread(handleClient, clientSocket));
 }
